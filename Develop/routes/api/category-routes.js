2c5210111dbf765fbb52ad3da484c46f7994c357
Brian Adams <bpeteradams@gmail.com>
2021-05-04 16:30:11 -0400

    wrote function to add new category to the db

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -34,8 +34,16 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create({
+      category_name: req.body.category_name
+    })
+    res.status(200).json(newCategory);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 router.put('/:id', (req, res) => {
