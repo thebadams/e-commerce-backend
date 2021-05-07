e687814d048c420b9eede90775e5098e762545e9
Brian Adams <bpeteradams@gmail.com>
2021-05-07 19:57:49 -0400

    wrote function to add a new tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -36,8 +36,16 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create({
+      tag_name: req.body.tag_name
+    })
+    res.status(200).json(newTag)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 router.put('/:id', (req, res) => {
